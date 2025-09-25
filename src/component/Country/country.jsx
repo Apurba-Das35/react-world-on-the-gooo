69cb448e952e115703ef5ff90ef4edfa6a8b3bbe
Apurbo-Das35 <apurbadas3511@gmail.com>
2025-09-26 01:20:53 +0600

    more react added

--- src/component/Country/country.jsx
+++ src/component/Country/country.jsx
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './country.css'
 
-const Country = ({country}) => {
-    console.log(country.name.common)
+const Country = ({country, handleVisitedCountries}) => {
+    // console.log(country.name.common)
+    const [visited, setVisited] = useState(false);
+
+    const handleVisited = () =>{
+        // if(visited){
+        //     setVisited(false)
+        // }
+        // else{
+        //     setVisited(true)
+        // }
+
+        // setVisited(visited ? false : true)
+
+        setVisited(!visited)
+        handleVisitedCountries(country)
+    }
     return (
-        <div>
+        <div className={`country ${visited && 'country-visited'}`}>
             <img src={country.flags.flags.png} alt={country.flags.flags.alt}/>
             <h3>Name: {country.name.common}</h3>
             <p>Population: {country.population.population}</p>
+            <p>Area: {country.area.area}</p>
+            <button onClick={handleVisited}>
+                {visited ? 'Visited' : 'Not Visited'}
+            </button>
         </div>
     );
 };
--- src/component/Countrys/Countries.jsx
+++ src/component/Countrys/Countries.jsx
@@ -1,16 +1,39 @@
-import React, { use } from 'react';
+import React, { use, useState } from 'react';
 import Country from '../Country/country';
+import './countries.css'
 
-const Countries = ({countriesPromise}) => {
+const Countries = ({ countriesPromise }) => {
+    const [visitedCountry, setVisitedCountry] = useState([])
+    const handleVisitedCountries = (country) =>{
+        console.log('visited country', country);
+        const newVisitedCountries = [...visitedCountry, country];
+        setVisitedCountry(newVisitedCountries);
+    }
+
+
     const countriesData = use(countriesPromise);
     const countries = countriesData.countries
-    
+
     return (
         <div>
             <h1>In the React World: {countries.length}</h1>
-            {
-                countries.map(country => <Country country={country}></Country>)
-            }
+            <h3>Total Visited Country: {visitedCountry.length}</h3>
+            <ol>
+                {
+                    visitedCountry.map(country => <li
+                    key={country.cca3.cca3}
+                    >{country.name.common}</li>)
+                }
+            </ol>
+            <div className='countries'>
+                {
+                    countries.map(country => <Country
+                        key={country.cca3.cca3}
+                        country={country}
+                        handleVisitedCountries={handleVisitedCountries}
+                    ></Country>)
+                }
+            </div>
         </div>
     );
 };
